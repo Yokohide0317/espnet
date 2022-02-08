c3afc4d4075662e3ea1a1bb92b996e83c238a3a1
Yokohide0317 <yokohide0317@gmail.com>
2022-02-08 18:59:21 +0900

    remove cpu

--- tools/installers/install_torch.sh
+++ tools/installers/install_torch.sh
@@ -1,1 +1,1 @@
-python3 -m pip install torch==1.10.2+cpu torchvision==0.11.3+cpu torchaudio==0.10.2+cpu -f https://download.pytorch.org/whl/cpu/torch_stable.html
+python3 -m pip install torch==1.10.2 torchvision==0.11.3 torchaudio==0.10.2 -f https://download.pytorch.org/whl/cpu/torch_stable.html
